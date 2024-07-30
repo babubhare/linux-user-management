cd64ee5b9e877d42a28a9bc8984103ace1e3f8ac
babubhare <babubhare@gmail.com>
2024-07-30 19:58:34 +0530

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -14,3 +14,5 @@ touch /etc/sudoers.d/dev
 echo "Defaults:${iuser} !fqdn" >> /etc/sudoers.d/${iuser}
 echo "Defaults:${iuser} !requiretty" >> /etc/sudoers.d/${iuser}
 echo "${iuser} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${iuser}
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== dev insecure public key" > "/home/${iuser}/.ssh/authorized_keys"
