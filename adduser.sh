e2d7803e5144cab179fa9eb72a7a36f7f4bf9dfa
babubhare <babubhare@gmail.com>
2024-07-30 19:49:46 +0530

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -3,8 +3,7 @@ iuser=dev
 ipasswd=dev
 
 adduser ${iuser} --gecos "dev,dev,dev,dev" --disabled-password
-passwd ${iuser} ${ipasswd}
-mkdir "/home/${iuser}/.ssh"
+passwd "$iuser" <<<"$ipasswd"$'\n'"$ipasswd"
 chmod 0700 "/home/${iuser}/.ssh"
 touch "/home/${iuser}/.ssh/authorized_keys"
 chmod 0600 "/home/${iuser}/.ssh/authorized_keys"
