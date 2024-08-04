2832cf0743dbd5d9617fe71bef78c8d45c4213b3
babubhare <babubhare@gmail.com>
2024-08-04 21:45:46 +0530

    Create changeRegularBashrc.sh

---
+++ changeRegularBashrc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+iuser=dev
+ipasswd=dev
+
+mkdir /etc/skel/.ssh
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== dev insecure public key" > "/home/${iuser}/.ssh/authorized_keys"
+
+# Bash file changes
+echo "# BABU CHANGES " >> /home/${iuser}/.bashrc
+echo "kernel_version=\$(uname -r)" >> /home/${iuser}/.bashrc
+echo "temp=\$(lsb_release -i)" >> /home/${iuser}/.bashrc
+echo "count=\${#temp}" >> /home/${iuser}/.bashrc
+echo "bc_distribution_id=\$(lsb_release -i | cut -c 17-\$count)" >> /home/${iuser}/.bashrc
+echo "temp=\$(lsb_release -i)" >> /home/${iuser}/.bashrc
+echo "count=\${#temp}" >> /home/${iuser}/.bashrc
+echo "bc_release=\$(lsb_release -r | cut -c 10-\$count)" >> /home/${iuser}/.bashrc
+echo "temp=\$(lsb_release -i)" >> /home/${iuser}/.bashrc
+echo "count=\${#temp}" >> /home/${iuser}/.bashrc
+echo "bc_codename=\$(lsb_release -c | cut -c 11-\$count)" >> /home/${iuser}/.bashrc
+echo "temp=\$(lsb_release -i)" >> /home/${iuser}/.bashrc
+echo "count=\${#temp}" >> /home/${iuser}/.bashrc
+echo "bc_systemname="v-lpic-\${bc_distribution_id}\${bc_release}\${bc_codename}\${kernel_version}"" >> /home/${iuser}/.bashrc
+echo "# END CHANGES" >> /root/.bashrc
+
+echo "color_prompt=yes" >> /home/${iuser}/.bashrc
+
+echo "if [ "\$color_prompt" = yes ]; then" >> /home/${iuser}/.bashrc
+    echo "PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\$bc_systemname\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /home/${iuser}/.bashrc
+echo "else" >> /home/${iuser}/.bashrc
+    echo "PS1='\${debian_chroot:+(\$debian_chroot)}\u@\$bc_systemname:\w\$ '" >> /home/${iuser}/.bashrc
+echo "fi" >> /home/${iuser}/.bashrc
+
+source /home/${iuser}/.bashrc
+sudo hostnamectl set-hostname ${bc_systemname}
