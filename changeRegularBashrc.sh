bf54e01a0b2321aa00087c523aa82ec7d39cf0a8
babubhare <babubhare@gmail.com>
2024-09-06 22:00:09 +0530

    Update changeRegularBashrc.sh

--- changeRegularBashrc.sh
+++ changeRegularBashrc.sh
@@ -2,7 +2,7 @@
 iuser=vagrant
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== dev insecure public key" > "/home/${iuser}/.ssh/authorized_keys"
-
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1YdxBpNlzxDqfJyw/QKow1F+wvG9hXGoqiysfJOn5Y vagrant insecure public key"
 # Bash file changes
 echo "# BABU CHANGES " >> /home/${iuser}/.bashrc
 echo "kernel_version=\$(uname -r)" >> /home/${iuser}/.bashrc
